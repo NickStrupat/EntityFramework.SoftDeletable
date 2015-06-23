ce9fe690d5c071240cc0ddf45b83a300c19e07b8
Nick Strupat <nick.strupat@gmail.com>
2015-06-23 00:34:10 -0400

    add extensions to control filtering by specific type

--- EntityFramework.SoftDeletable/DbContextExtensions.cs
+++ EntityFramework.SoftDeletable/DbContextExtensions.cs
@@ -9,14 +9,26 @@ namespace EntityFramework.SoftDeletable {
 
         public static void EnableSoftDeletableFilter(this DbContext dbContext) {
             dbContext.EnableFilter(SoftDeletableFilterName);
-		}
+        }
 
-		public static void DisableSoftDeletableFilter(this DbContext dbContext) {
-			dbContext.DisableFilter(SoftDeletableFilterName);
-		}
+        public static void DisableSoftDeletableFilter(this DbContext dbContext) {
+            dbContext.DisableFilter(SoftDeletableFilterName);
+        }
 
-		public static void AddSoftDeletableFilter(this ConventionsConfiguration conventions) {
+        public static void AddSoftDeletableFilter(this ConventionsConfiguration conventions) {
             conventions.Add(FilterConvention.Create(SoftDeletableFilterName, SoftDeletable.IsNotDeletedExpression));
+        }
+
+        public static void EnableSoftDeletableFilter<TSoftDeletable>(this DbContext dbContext) where TSoftDeletable : ISoftDeletable {
+            dbContext.EnableFilter(SoftDeletableFilterName + typeof(TSoftDeletable).Name);
+        }
+
+        public static void DisableSoftDeletableFilter<TSoftDeletable>(this DbContext dbContext) where TSoftDeletable : ISoftDeletable {
+            dbContext.DisableFilter(SoftDeletableFilterName + typeof(TSoftDeletable).Name);
+        }
+
+        public static void AddSoftDeletableFilter<TSoftDeletable>(this ConventionsConfiguration conventions) where TSoftDeletable : ISoftDeletable {
+            conventions.Add(FilterConvention.Create(SoftDeletableFilterName + typeof(TSoftDeletable).Name, SoftDeletable.IsNotDeletedExpression));
         }
     }
 }
