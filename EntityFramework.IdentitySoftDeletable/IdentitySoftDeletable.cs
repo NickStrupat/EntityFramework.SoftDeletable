e604ab61e67f7259d9dfc6abfd3cfe2d7a2cd453
NickStrupat <nick.strupat@gmail.com>
2015-09-06 15:45:59 -0400

    add soft deletable base classes which retrieve the current Identity user's ID automatically

---
+++ EntityFramework.IdentitySoftDeletable/IdentitySoftDeletable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using EntityFramework.SoftDeletable;
+using Microsoft.AspNet.Identity;
+
+namespace EntityFramework.IdentitySoftDeletable {
+	public abstract class IdentitySoftDeletable<T> : UserSoftDeletable<T> where T : IConvertible {
+		public sealed override T GetCurrentUserId() {
+			return System.Web.HttpContext.Current.User.Identity.GetUserId<T>();
+		}
+	}
+}
---
+++ EntityFramework.VersionedIdentitySoftDeletable/VersionedIdentitySoftDeletable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using EntityFramework.VersionedSoftDeletable;
+using Microsoft.AspNet.Identity;
+
+namespace EntityFramework.VersionedIdentitySoftDeletable
+{
+    public class VersionedIdentitySoftDeletable<TUserId, TVersionedUserDeleted> : VersionedUserSoftDeletable<TUserId, TVersionedUserDeleted> where TUserId : IConvertible {
+	    public sealed override TUserId GetCurrentUserId() {
+		    return System.Web.HttpContext.Current.User.Identity.GetUserId<TUserId>();
+	    }
+    }
+}
