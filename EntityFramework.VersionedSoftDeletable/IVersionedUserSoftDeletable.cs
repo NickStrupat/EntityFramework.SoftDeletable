a12cf700123f726534f6c084876686956cbce265
NickStrupat <nick.strupat@gmail.com>
2015-09-06 15:45:09 -0400

    clean up some commented code

--- EntityFramework.VersionedSoftDeletable/IVersionedUserSoftDeletable.cs
+++ EntityFramework.VersionedSoftDeletable/IVersionedUserSoftDeletable.cs
@@ -4,21 +4,18 @@ using System.Data.Entity;
 using EntityFramework.VersionedProperties;
 
 namespace EntityFramework.VersionedSoftDeletable {
-	public interface IVersionedUserSoftDeletable<TUserId, TVersionedUserDeleted/*, TUserDeleted, TUserDeletedVersion, TIUserDeleteds*/> : IVersionedProperties
-		//where TVersionedUserDeleted : VersionedBase<TUserDeleted, TUserDeletedVersion, TIUserDeleteds>
-		//where TUserDeletedVersion : VersionBase<TUserDeleted>, new()
-	{
+	public interface IVersionedUserSoftDeletable<out TUserId, out TVersionedUserDeleted> : IVersionedProperties {
 		TVersionedUserDeleted Deleted { get; }
 		TUserId GetCurrentUserId();
 	}
 
-	public interface IVersionedUserSoftDeletable : IVersionedUserSoftDeletable<String, VersionedUserDeleted/*, UserDeleted, UserDeletedVersion, IUserDeleteds*/> {}
+	public interface IVersionedUserSoftDeletable : IVersionedUserSoftDeletable<String, VersionedUserDeleted> { }
 
 	public abstract class UserDeleted<TUserId> {
 		public TUserId UserId { get; private set; } // We're unable to apply a foreign constraint here due to current limitations of complex types in Entity Framework 6
 		public Boolean IsDeleted { get; private set; }
 
-		protected UserDeleted() {}
+		protected UserDeleted() { }
 		protected UserDeleted(TUserId userId, Boolean isDeleted) {
 			UserId = userId;
 			IsDeleted = isDeleted;
@@ -33,10 +30,10 @@ namespace EntityFramework.VersionedSoftDeletable {
 
 	[ComplexType]
 	public class UserDeleted : UserDeleted<String> {
-		internal UserDeleted(String userId, Boolean isDeleted) : base(userId, isDeleted) {}
+		internal UserDeleted(String userId, Boolean isDeleted) : base(userId, isDeleted) { }
 	}
 
-	public class UserDeletedVersion : VersionBase<UserDeleted> {}
+	public class UserDeletedVersion : VersionBase<UserDeleted> { }
 
 	public interface IUserDeleteds {
 		DbSet<UserDeletedVersion> UserDeleteds { get; set; }
--- EntityFramework.VersionedSoftDeletable/VersionedUserSoftDeletableInnerTypes.cs
+++ EntityFramework.VersionedSoftDeletable/VersionedUserSoftDeletableInnerTypes.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using EntityFramework.VersionedProperties;
 
 namespace EntityFramework.VersionedSoftDeletable {
